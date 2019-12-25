43190ba73149f1d34d264043ed9bf14e2c3b227f
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2019-12-25 14:24:09 +0900

    mod

--- pkg/pagekite/pagekite.go
+++ pkg/pagekite/pagekite.go
@@ -1,15 +1,17 @@
 package pagekite
 
 import (
-	"time"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/takutakahashi/pagekite-ingress-controller/pkg/pagekite/types"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/api/extensions/v1beta1"
-	"k8s.io/apimachinery/pkg/fields"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/cache"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 type PageKite struct {
@@ -28,45 +30,49 @@ func NewPageKite() PageKite {
 	}
 	return pk
 }
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
+}
 
 func (pk *PageKite) StartObserver() error {
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err)
+	var kubeconfig *string
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//	config, err = rest.InClusterConfig()
+	//	if err != nil {
+	//		panic(err)
+	//	}
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err)
 	}
 
-	watchlist := cache.NewListWatchFromClient(
-		clientset.CoreV1().RESTClient(),
-		"ingresses",
-		v1.NamespaceAll,
-		fields.Everything(),
-	)
-
-	_, controller := cache.NewInformer(
-		watchlist,
-		&v1beta1.Ingress{},
-		0,
-		cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				pk.Ingress.Add(obj)
-				pk.Reload <- struct{}{}
-			},
-			DeleteFunc: func(obj interface{}) {
-				pk.Ingress.Delete(obj)
-				pk.Reload <- struct{}{}
-			},
-			UpdateFunc: func(oldObj, newObj interface{}) {
-				pk.Ingress.Update(oldObj, newObj)
-				pk.Reload <- struct{}{}
-			},
-		},
-	)
-	go controller.Run(pk.Stop)
+	serviceStreamWatcher, err := clientset.CoreV1().Services("").Watch(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
 	for {
-		time.Sleep(time.Second)
+		select {
+		case event := <-serviceStreamWatcher.ResultChan():
+			service := event.Object.(*v1.Service)
+
+			for key, value := range service.Labels {
+				fmt.Printf("Key, VAlue: %s %s\n", key, value)
+			}
+		}
 	}
 }
