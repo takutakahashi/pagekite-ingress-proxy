454b14a0b5327010ec1ee917b4fdf0a40bf37bf1
takutaka <takutaka@pepabo.com>
2019-12-27 16:08:11 +0900

    add ing

--- pkg/pagekite/types/types.go
+++ pkg/pagekite/types/types.go
@@ -6,14 +6,19 @@ import (
 
 	"github.com/leekchan/gtf"
 	v1 "k8s.io/api/core/v1"
+	extv1beta1 "k8s.io/api/extensions/v1beta1"
 )
 
 type PageKiteConfig struct {
-	Name                  string
-	Secret                string
-	Cache                 []byte
-	ControllerService     v1.Service
-	ControllerServiceName string
+	Name     string
+	Secret   string
+	Cache    []byte
+	Resource PageKiteResource
+}
+
+type PageKiteResource struct {
+	IngressControllerService v1.Service
+	Ingresses                []extv1beta1.Ingress
 }
 
 func (pkc *PageKiteConfig) GenerateConfig() []byte {
@@ -27,7 +32,7 @@ func (pkc *PageKiteConfig) GenerateConfig() []byte {
 		C PageKiteConfig
 		S v1.Service
 	}
-	err = tmpl.Execute(&buf, pkset{C: *pkc, S: pkc.ControllerService})
+	err = tmpl.Execute(&buf, pkset{C: *pkc, S: pkc.Resource.IngressControllerService})
 	if err != nil {
 		log.Println(err)
 		return []byte{}
